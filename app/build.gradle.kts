9c5295cde47b09155ac70aa25ead507061165593
Shyak Das <shayak.das007@gmail.com>
2026-09-10 00:24:51 +0530

    chore(app): add launch splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,6 +130,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
     testImplementation(kotlin("test"))
